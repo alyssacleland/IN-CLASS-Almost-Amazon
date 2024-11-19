c7b8bdff4626c1065f513b0a50e715fd799c1da5
Alyssa Cleland <alyssamariecleland@gmail.com>
2024-11-18 21:04:48 -0600

    functionality and styling of view author details and books

--- pages/viewAuthor.js
+++ pages/viewAuthor.js
@@ -1,42 +1,32 @@
 // note to self: i created this file
 
-import clearDom from '../utils/clearDom';
+// import clearDom from '../utils/clearDom';
 import renderToDOM from '../utils/renderToDom';
-// import { showBooks } from './books';
+import { showAuthorBooks } from './books';
 
 const viewAuthor = (data) => {
-  clearDom();
+  document.querySelector('#view').innerHTML = '';
+  document.querySelector('#store').innerHTML = '';
 
   const { author, books } = data; // destructuring the data to get the author and book arrays. this is shorthand for const author = data.author, and likewise for books.
 
   // add author details to dom string
-  let domString = `
-  <div class="card" style="width: 18rem;">
-    <div class="card-body">
-     <p class="card-text bold">${author.favorite ? '<span class="badge badge-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i>Favorited</span>' : ''}</p>
-          <hr>
-      <h5 class="card-title">${author.first_name} ${author.last_name}</h5>
-      <h6 class="card-subtitle mb-2 text-muted">${author.email}</h6>
-      <hr>
-      <i class="fas fa-edit btn btn-info" id="update-author--${author.firebaseKey}"></i>
-      <i class="btn btn-danger fas fa-trash-alt" id="delete-author-btn--${author.firebaseKey}"></i>
-      <h6>Books:</h6>
-      <ul>`;
 
-  // Loop through the books array and append each book's title to the list
-  books.forEach((book) => {
-    domString += `
-      <li>${book.title}</li>
-    `;
-  });
-
-  // Close the unordered list and card body
-  domString += `
-      </ul>
-    </div>
-  </div>`;
+  const domString = `
+   <h5>Books</h5>
+  <div class="text-white ms-5 details">
+    <h5>${author.first_name} ${author.last_name} ${author.favorite ? '<span class="badge bg-danger"><i class="fa fa-heart" aria-hidden="true"></i></span>' : ''}</h5>
+    Author Email: <a href="mailto:${author.email}">${author.email}</a>
+    <div> <i class="fas fa-edit btn btn-info" id="update-author--${author.firebaseKey}"></i>
+      <i class="btn btn-danger fas fa-trash-alt" id="delete-author-btn--${author.firebaseKey}"></i> </div>
+    <hr>
+    <h5>Books</h5>
+    <div id="books-container"><!-- placeholder for author's books --></div>`;
 
   renderToDOM('#view', domString);
-};
 
+  // add books to #books-container
+  const booksHtml = showAuthorBooks(books);
+  document.querySelector('#books-container').innerHTML = booksHtml;
+};
 export default viewAuthor;
