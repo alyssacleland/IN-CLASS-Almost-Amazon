2b684f9fa996ecb459f3b2c85e732b10b24a8e79
Alyssa Cleland <alyssamariecleland@gmail.com>
2024-11-23 14:14:56 -0600

    functional search bar

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 
 import 'bootstrap'; // import bootstrap elements and js
+// import 'bootstrap-icons/font/bootstrap-icons.css'; // import bootstrap icons (so my magnifying glass will work?). note: i had to install first using "npm install bootstrap-icons". or what showed up in problems "npm i -S bootstrap-icons" nvm this isn't working
 import '../styles/main.scss';
 
 const init = () => {
